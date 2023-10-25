2494a11c7502535d44d0b482c54cdaee8957fd22
Mihai Gheorghe <mihai.gheorghe@gdm.ro>
2023-10-25 13:28:13 +0300

    Remove node_modules directory

--- API/app.js
+++ API/app.js
@@ -20,6 +20,16 @@ const logger = require('morgan');
 const cors = require('cors');
 const port = 3000;
 
+const fs = require('fs')
+
+const bcrypt = require('bcrypt');
+const saltRounds = 10;
+
+let jwt = require('jsonwebtoken');
+
+const privateKey = fs.readFileSync('private.key');
+const publicKey = fs.readFileSync('public.key');
+
 app.use(logger('dev'));
 app.use(cors()) //see more at https://www.npmjs.com/package/cors
 app.use(express.urlencoded({ extended: false }))
@@ -40,13 +50,55 @@ app.post('/register', (req, res) => {
   if (filteredUsers.length) {
     res.send('This user already exits')
   } else {
-    user.id = users[users.length - 1].id + 1
-    users.push(user)
-    console.log('DB has been updated: ', users)
-    res.send('The user has been successfully added')
+    
+    bcrypt.hash(user.password, saltRounds, function(err, hash) {
+        // Store hash in your password DB.
+        user.password = hash
+        user.id = users[users.length - 1].id + 1
+        users.push(user)
+        console.log('DB has been updated: ', users)
+        res.send('The user has been successfully added')
+    });
   }
 
 });
+
+app.post ('/login', (req, res) => {
+    let user = req.body
+    console.log('trying to login with: ', user)
+
+    const filteredUsers = users.filter(function(dbUser) {
+        return dbUser.email === user.email
+      })
+
+    if (filteredUsers.length) {
+        let dbHash = filteredUsers[0].password
+
+        bcrypt.compare(user.password, dbHash, function(err, result) {
+            if (result) {
+                let token = jwt.sign(user.email, privateKey, { 
+                  algorithm: 'RS256',
+                  // jwa: 'RSA-OAEP-256'
+                })
+
+                res.json({
+                  token: token,
+                  name: filteredUsers[0].name
+                })
+            } else {
+                res.status(401).json({
+                  error: 'Wrong password'
+                })
+            }
+        });
+
+    } else {
+        res.status(401).json({
+          error: 'The user doesn\'t exist'
+        })
+    }
+
+})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}!`)
--- CLIENT/Users/login.js
+++ CLIENT/Users/login.js
@@ -1,32 +1,45 @@
-import { users } from '../Database/users.js'
+let url = "http://localhost:3000/"
 
 export function login(e) {
     e.preventDefault()
-    const loginEmail = document.getElementById("loginEmail")
-    const loginPassword = document.getElementById("loginPassword")
-    console.log(`vrei sa te autentifici cu ${loginEmail.value} si ${loginPassword.value}`)
 
-    const user = users.filter(function(user) {
-        return user.email === loginEmail.value;
-      });
+    let user = {}
 
-    if (user[0]) {
-        console.log('utilizator gasit: ', user[0])
-        if (user[0].password === loginPassword.value) {
-            localStorage.setItem('isLoggedIn', true)
-            localStorage.setItem('username', user[0].name)
-            document.getElementById('login').setAttribute('hidden', 'true')
-            document.getElementById('register').setAttribute('hidden', 'true')
-            document.getElementById('user').removeAttribute('hidden')
-            document.getElementById('greetings').textContent = 'Salut ' + user[0].name
+    user.email = document.getElementById('loginEmail').value
+    user.password = document.getElementById('loginPassword').value
 
-            let taskButtons = document.querySelectorAll('.taskButtons')
-            taskButtons.forEach(element => {
-                element.style.display = 'block'
-            });
-        }
-    } else
-    {
-        console.log('utilizatorul nu exista')
-    }
+    console.log('trying to login with ', user)
+    fetch(url + 'login', {
+        method: 'POST', 
+        mode: 'cors', 
+        cache: 'no-cache', 
+        credentials: 'same-origin', 
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        redirect: 'follow', 
+        referrerPolicy: 'no-referrer', 
+        body: JSON.stringify(user) //data format must be the same as in header
+        })
+        .then(res => res.json()
+        .then(res => {
+            if (res.error) {
+                console.log(res.error)
+            } else {
+                localStorage.setItem('isLoggedIn', true)
+                localStorage.setItem('authToken', res.token)
+                console.log('Suceessfully authenticated')
+                
+                document.getElementById('login').setAttribute('hidden', 'true')
+                document.getElementById('register').setAttribute('hidden', 'true')
+                document.getElementById('user').removeAttribute('hidden')
+                document.getElementById('greetings').textContent = 'Salut ' + res.name
+
+                let taskButtons = document.querySelectorAll('.taskButtons')
+                taskButtons.forEach(element => {
+                    element.style.display = 'block'
+                });
+            }
+        })
+        )
 }
